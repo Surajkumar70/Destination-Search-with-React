6872a860073d8778ac0de536afd9dc85a354453c
Surajkumar70 <name>
2022-07-17 08:45:36 +0530

    search somthing

---
+++ src/data.js
@@ -0,0 +1,119 @@
+import Surajkumar from './img/Surajkumar.jpeg'
+import Bhagsunag from './img/Bhagsunag.jpeg'
+import cricket   from './img/cricket.jpeg'
+import DalLake from './img/DalLake.jpeg'
+import NaddiView from './img/NaddiView.jpeg'
+import SunsetView from './img/SunsetView.jpeg'
+import TeaGarden from './img/TeaGarden.jpeg'
+import triund from './img/triund.jpeg'
+import Ropeway from './img/Ropeway.jpeg'
+import City from './img/City.jpg'
+import Waterpark from './img/Waterpark.jpeg'
+import Baijnath from './img/Baijnath.jpeg'
+import Lotus from './img/Lotus.jpeg'
+import RedFort from './img/RedFort.jpeg'
+import DharamshalaNature from './img/DharamshalaNature.jpeg'
+import DalaiLama from './img/DalaiLama.jpeg'
+
+
+
+
+ 
+
+const data = {
+    cardData: [
+        {
+            id: 1,
+            img: Surajkumar,
+            title: 'Surajkumar',
+            desc: 'My Profile pic'
+        }, 
+        {
+            id: 2,
+            img: Bhagsunag,
+            title: 'water Fall',
+            desc: 'Bhagsunag'
+        }, 
+        {
+            id: 3,
+            img: cricket,
+            title: 'Cricket Stadium',
+            desc: 'Dharamshala'
+        }, 
+        {
+            id: 4,
+            img: DalLake,
+            title: 'Dal Lake',
+            desc: 'Naddi'
+        }, {
+            id: 5,
+            img: NaddiView,
+            title: 'Naddi',
+            desc: 'Naddi'
+        }, {
+            id: 6,
+            img: SunsetView,
+            title: 'SunSetView',
+            desc: 'Mountain'
+        }, {
+            id: 7,
+            img: TeaGarden,
+            title: 'Tea Garden',
+            desc: 'PalamPur'
+        }, {
+            id: 8,
+            img: triund,
+            title: 'Triund ',
+            desc: ' Dharamkot'
+        }, {
+            id: 9,
+            img: Ropeway,
+            title: 'Rope Way',
+            desc: 'Dharamshala'
+        }, {
+            id: 10,
+            img: City,
+            title: 'City',
+            desc: 'London'
+        }, 
+
+        {
+            id: 11,
+            img: Waterpark,
+            title: 'WaterPark',
+            desc: 'WaterPark'
+        },
+        {
+            id: 12,
+            img: Baijnath,
+            title: 'BaijNath',
+            desc: 'Temple'
+        }, 
+        {
+            id: 13,
+            img: Lotus,
+            title: 'Loutus',
+            desc: 'Temple'
+        }, 
+        {
+            id: 14,
+            img: RedFort,
+            title: 'RedFort',
+            desc: 'Delhi'
+        },
+        {
+            id: 15,
+            img: DharamshalaNature,
+            title: 'Nature of Beauty',
+            desc: 'Dharamshala'
+        },
+        {
+            id: 16,
+            img: DalaiLama,
+            title: 'DalaiLama',
+            desc: 'McLeod Ganj'
+        }
+    ]
+}
+
+export default data;
